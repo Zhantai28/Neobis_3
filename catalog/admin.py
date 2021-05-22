8d0df125bffd678204e423965bb08cc286c56e8c
Zhantai <Zhantai.Ismailov@gmail.com>
2021-05-22 13:18:25 +0600

    added some templates and static

--- catalog/admin.py
+++ catalog/admin.py
@@ -23,13 +23,6 @@ class BooksInline(admin.TabularInline):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    """Administration object for Author models.
-    Defines:
-     - fields to be displayed in list view (list_display)
-     - orders fields in detail view (fields),
-       grouping the date fields horizontally
-     - adds inline addition of books in author view (inlines)
-    """
     list_display = ('last_name',
                     'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
@@ -42,11 +35,6 @@ class BooksInstanceInline(admin.TabularInline):
 
 
 class BookAdmin(admin.ModelAdmin):
-    """Administration object for Book models.
-    Defines:
-     - fields to be displayed in list view (list_display)
-     - adds inline addition of book instances in book view (inlines)
-    """
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
@@ -56,12 +44,6 @@ admin.site.register(Book, BookAdmin)
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    """Administration object for BookInstance models.
-    Defines:
-     - fields to be displayed in list view (list_display)
-     - filters that will be displayed in sidebar (list_filter)
-     - grouping of fields into sections (fieldsets)
-    """
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
